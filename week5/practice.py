9a85883bbf2216ff174336acbabf610dcac4b03c
Raiymbek <mibuhic773@mail.ru>
2021-03-01 20:48:33 +0600

    w5

---
+++ week5/practice.py
@@ -0,0 +1,8 @@
+import os
+import shutil
+
+# shutil.move('week5/dir1/allfiles.py', 'week5/')
+# os.remove('lol.txt')
+
+# os.rename('week5/1.py', 'practice.py')
+shutil.move('practice.py', 'week5/')
